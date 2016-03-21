aca018014945543ab86bea947e47aaba0ae36793
xuesea <xuesea@gmail.com>
2016-03-21 18:05:11 +0800

    modify code with suggested format.

--- 1-50/003Longest Substring Without Repeating Characters/longest_substring_without_repeat.js
+++ 1-50/003Longest Substring Without Repeating Characters/longest_substring_without_repeat.js
@@ -1,24 +1,33 @@
 // Source : https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
 // Author : Lyndon
 // Date   : 2016-03-14
-var str2="asdfadfaasdfasdfasdfadsfasdf";
-+function (str) {
-    var i= 0,longest_repeat=[],ost=0;
-   while(i<str.length){
-       for(var j=i;j<str.length;j++){
-           var reg_str=str.substring(i,j+1);
-           var leave_str=str.substring(j+1,str.length);
-           if(leave_str.includes(reg_str)){
-               var len=reg_str.length;
-               if(len>=ost) {
-                   longest_repeat.push(reg_str);
-                   ost = len;
-               }
-           }else {
-               break;
-           }
-       }
-       i++;
-   }
-    console.log(longest_repeat[longest_repeat.length-1]);
-}(str2);
+
+/**********************************************************************************
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc",
+ * which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ * 648 ms
+ **********************************************************************************/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function(s) {
+    var temp="",a=0,b=0;
+    for(var j=0;j<s.length;j++) {
+        for (var i = j; i < s.length; i++) {
+            if (temp.indexOf(s[i]) >= 0) {
+                b = a > b ? a : b;
+                a = 0;
+                temp = "";
+                break;
+            } else {
+                temp += s[i];
+                a++;
+            }
+        }
+    }
+    return a>b?a:b;
+};
